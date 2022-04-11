44855821611457accc378d8b4bc5d23a1a266347
Shrirang Dharkar <dharkarshrirang@gmail.com>
2022-04-11 16:35:17 +0530

    Comitting updated API-gateway

--- api gate/src/main/java/com/api/gateway/ApiGatewayApplication.java
+++ api gate/src/main/java/com/api/gateway/ApiGatewayApplication.java
@@ -2,10 +2,12 @@ package com.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@RequestMapping("/gateway")
+@EnableEurekaClient
+//@RequestMapping("/gateway")
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
